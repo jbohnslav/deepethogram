e28b7ee8e5b2b897e00daf31a941cb71d0b86e0f
Jim Robinson-Bohnslav <jbohnslav@gmail.com>
2020-12-07 14:46:36 -0500

    added print_hdf5

--- deepethogram/utils.py
+++ deepethogram/utils.py
@@ -10,6 +10,7 @@ from types import SimpleNamespace
 from typing import Union
 
 import cv2
+import h5py
 import numpy as np
 import torch
 import yaml
@@ -214,9 +215,11 @@ def save_dict_to_yaml(dictionary: dict, filename: Union[str, bytes, os.PathLike]
         yaml.dump(dictionary, f, default_flow_style=False)
 
 
-def tensor_to_np(tensor: torch.Tensor) -> np.ndarray:
+def tensor_to_np(tensor: Union[torch.Tensor, np.ndarray]) -> np.ndarray:
     """Simple function for turning pytorch tensor into numpy ndarray"""
-    return tensor.to('cpu').detach().numpy()
+    if type(tensor) == np.ndarray:
+        return tensor
+    return tensor.cpu().detach().numpy()
 
 
 def in_this_dir(abs_path: Union[str, os.PathLike]) -> dict:
@@ -743,3 +746,49 @@ def get_subfiles(root: Union[str, bytes, os.PathLike], return_type: str = None) 
     elif return_type == 'directory':
         files = [i for i in files if os.path.isdir(i)]
     return files
+
+def print_hdf5(h5py_obj, level=-1, print_full_name: bool = False, print_attrs: bool = True) -> None:
+    """ Prints the name and shape of datasets in a H5py HDF5 file.
+    Parameters
+    ----------
+    h5py_obj: [h5py.File, h5py.Group]
+        the h5py.File or h5py.Group object
+    level: int
+        What level of the file tree you are in
+    print_full_name
+        If True, the full tree will be printed as the name, e.g. /group0/group1/group2/dataset: ...
+        If False, only the current node will be printed, e.g. dataset:
+    print_attrs
+        If True: print all attributes in the file
+    Returns
+    -------
+    None
+    """
+    def is_group(f):
+        return type(f) == h5py._hl.group.Group
+
+    def is_dataset(f):
+        return type(f) == h5py._hl.dataset.Dataset
+
+    def print_level(level, n_spaces=5) -> str:
+        if level == -1:
+            return ''
+        prepend = '|' + ' ' * (n_spaces - 1)
+        prepend *= level
+        tree = '|' + '-' * (n_spaces - 2) + ' '
+        return prepend + tree
+
+    for key in h5py_obj.keys():
+        entry = h5py_obj[key]
+        name = entry.name if print_full_name else os.path.basename(entry.name)
+        if is_group(entry):
+            print('{}{}'.format(print_level(level), name))
+            print_hdf5(entry, level + 1, print_full_name=print_full_name)
+        elif is_dataset(entry):
+            shape = entry.shape
+            dtype = entry.dtype
+            print('{}{}: {} {}'.format(print_level(level), name,
+                                       shape, dtype))
+    if level == -1:
+        if print_attrs:
+            print('attrs: ')
