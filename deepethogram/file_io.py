cec54f7c0711942acd1af891dc56c48cafef1abf
Jim Robinson-Bohnslav <jbohnslav@gmail.com>
2021-01-28 11:14:27 -0500

    fixed movie conversion

--- deepethogram/file_io.py
+++ deepethogram/file_io.py
@@ -70,9 +70,24 @@ def read_label_csv(labelfile):
     return label
 
 
-def convert_video(videofile: Union[str, os.PathLike], movie_format: str, *args, **kwargs) -> None:
+def convert_video(videofile: Union[str, os.PathLike], movie_format: str, *args,
+                  **kwargs) -> None:
     with VideoReader(videofile) as reader:
-        with VideoWriter(videofile, movie_format=movie_format, *args, **kwargs) as writer:
+        basename = os.path.splitext(videofile)[0]
+        if movie_format == 'ffmpeg':
+            out_filename = basename + '.mp4'
+        elif movie_format == 'opencv':
+            out_filename = basename + '.avi'
+        elif movie_format == 'hdf5':
+            out_filename = basename + '.h5'
+        elif movie_format == 'directory':
+            out_filename = basename
+        else:
+            raise ValueError('unexpected value of movie format: {}'.format(movie_format))
+        with VideoWriter(out_filename,
+                         movie_format=movie_format,
+                         *args,
+                         **kwargs) as writer:
             for frame in reader:
                 writer.write(frame)
 
