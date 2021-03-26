3f441ed3aaf93f829edd8f6cad8f9f4ac45b3a8a
Jim Bohnslav <jbohnslav@gmail.com>
2021-03-26 11:04:36 -0400

    removed imports from init for out-of-order loading issues

--- deepethogram/__init__.py
+++ deepethogram/__init__.py
@@ -1,7 +1,7 @@
 # from deepethogram import feature_extractor, flow_generator, gui, sequence, dataloaders, metrics, utils, viz, zscore
 # from deepethogram import feature_extractor, flow_generator, gui, sequence, dataloaders,
-from deepethogram.flow_generator.train import flow_generator_train
-from deepethogram.feature_extractor.train import feature_extractor_train
-from deepethogram.feature_extractor.inference import feature_extractor_inference
-from deepethogram.sequence.train import sequence_train
-from deepethogram.sequence.inference import sequence_inference
+# from deepethogram.flow_generator.train import flow_generator_train
+# from deepethogram.feature_extractor.train import feature_extractor_train
+# from deepethogram.feature_extractor.inference import feature_extractor_inference
+# from deepethogram.sequence.train import sequence_train
+# from deepethogram.sequence.inference import sequence_inference
