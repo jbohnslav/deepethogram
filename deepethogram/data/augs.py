9f305fe3495c6b8a31d57be6717c899061b7278d
Jim Bohnslav <jbohnslav@gmail.com>
2021-03-26 11:05:40 -0400

    updated for kornia 0.5

--- deepethogram/data/augs.py
+++ deepethogram/data/augs.py
@@ -4,7 +4,8 @@ from pprint import pformat
 import cv2
 import numpy as np
 import torch
-from kornia import augmentation as K, adjust_brightness, adjust_contrast, adjust_saturation, adjust_hue, pi
+from kornia import augmentation as K # , adjust_brightness, adjust_contrast, adjust_saturation, adjust_hue, pi
+from kornia.augmentation.container import VideoSequential
 from omegaconf import DictConfig
 from opencv_transforms import transforms
 from torch import nn
@@ -54,8 +55,11 @@ class NormalizeVideo(nn.Module):
 
         self.mean = mean.reshape(1, -1, 1, 1, 1)
         self.std = std.reshape(1, -1, 1, 1, 1)
+        self.ndim = self.mean.ndim
+        
+        assert self.ndim == self.std.ndim
 
-        self.normalize = K.Normalize(mean=mean, std=std)
+        # self.normalize = K.Normalize(mean=mean, std=std)
 
     #     def forward(self, tensor):
     #         if self.mean.device != tensor.device:
@@ -64,8 +68,16 @@ class NormalizeVideo(nn.Module):
     #             self.std = self.std.to(tensor.device)
 
     #         return (tensor - self.mean) / self.std
+    def normalize(self, tensor):
+        assert tensor.ndim == self.ndim
+        if self.mean.device != tensor.device:
+            self.mean = self.mean.to(tensor.device)
+        if self.std.device != tensor.device:
+            self.std = self.std.to(tensor.device)
+        return (tensor.float() - self.mean) / self.std
+    
     def forward(self, tensor):
-        return self.normalize(tensor.transpose(1, 2)).transpose(1, 2)
+        return self.normalize(tensor)
 
 
 class DenormalizeVideo(nn.Module):
@@ -82,18 +94,18 @@ class DenormalizeVideo(nn.Module):
         self.mean = mean.reshape(1, -1, 1, 1, 1)
         self.std = std.reshape(1, -1, 1, 1, 1)
 
-        self.normalize = K.Denormalize(mean=mean, std=std)
+        # self.normalize = K.Denormalize(mean=mean, std=std)
 
-        #     def forward(self, tensor):
-        #         if self.mean.device != tensor.device:
-        #             self.mean = self.mean.to(tensor.device)
-        #         if self.std.device != tensor.device:
-        #             self.std = self.std.to(tensor.device)
+    def normalize(self, tensor):
+        if self.mean.device != tensor.device:
+            self.mean = self.mean.to(tensor.device)
+        if self.std.device != tensor.device:
+            self.std = self.std.to(tensor.device)
 
-        #         return torch.clamp( tensor*self.std + self.mean , 0, 1)
+        return torch.clamp( tensor*self.std + self.mean , 0, 1)
 
     def forward(self, tensor):
-        return self.normalize(tensor.transpose(1, 2)).transpose(1, 2)
+        return self.normalize(tensor)
 
 
 class ToFloat(nn.Module):
@@ -109,116 +121,120 @@ class ToFloat(nn.Module):
         return self.__class__.__name__ + '()'
 
 
-class RandomColorJitterVideo(nn.Module):
-    """Wrapper for applying random color jitter to video clips
-    """
-    def __init__(self, brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1, return_transform=False,
-                 same_on_batch=False, p=0.5):
-        """Constructor for gpu augmentations for color jittering
+# class RandomColorJitterVideo(nn.Module):
+#     """Wrapper for applying random color jitter to video clips
+#     """
+#     def __init__(self, brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1, return_transform=False,
+#                  same_on_batch=False, p=0.5):
+#         """Constructor for gpu augmentations for color jittering
 
-        Parameters
-        ----------
-        brightness : float, optional
-            Strength of brightness augmentation, by default 0.1
-        contrast : float, optional
-            Strength of contrast augmentation, by default 0.1
-        saturation : float, optional
-            Strength of saturation augmentation, by default 0.1
-        hue : float, optional
-            Strength of hue augmentation, by default 0.1
-        return_transform : bool, optional
-            If True, returns transformation matrix, which will always be identity, by default False
-        same_on_batch : bool, optional
-            If True, applies the same augmentation to every batch element, by default False
-        p : float, optional
-            Probability of applying any augmentation to a given batch example, by default 0.5
-        """
-        super().__init__()
-        self.p = p
-        self.jitter_2d = K.ColorJitter(brightness=brightness, contrast=contrast, saturation=saturation,
-                                       hue=hue, return_transform=return_transform,
-                                       same_on_batch=same_on_batch, p=p)
+#         Parameters
+#         ----------
+#         brightness : float, optional
+#             Strength of brightness augmentation, by default 0.1
+#         contrast : float, optional
+#             Strength of contrast augmentation, by default 0.1
+#         saturation : float, optional
+#             Strength of saturation augmentation, by default 0.1
+#         hue : float, optional
+#             Strength of hue augmentation, by default 0.1
+#         return_transform : bool, optional
+#             If True, returns transformation matrix, which will always be identity, by default False
+#         same_on_batch : bool, optional
+#             If True, applies the same augmentation to every batch element, by default False
+#         p : float, optional
+#             Probability of applying any augmentation to a given batch example, by default 0.5
+#         """
+#         super().__init__()
+#         self.p = p
+#         self.jitter_2d = K.ColorJitter(brightness=brightness, contrast=contrast, saturation=saturation,
+#                                        hue=hue, return_transform=return_transform,
+#                                        same_on_batch=same_on_batch, p=p)
         
-        self.brightness = brightness
-        self.contrast = contrast
-        self.saturation = saturation
-        self.hue = hue
+#         self.brightness = brightness
+#         self.contrast = contrast
+#         self.saturation = saturation
+#         self.hue = hue
         
-        self.aug_values = {0: self.brightness, 
-                           1: self.contrast, 
-                           2: self.saturation, 
-                           3: self.hue}
+#         self.aug_values = {0: self.brightness, 
+#                            1: self.contrast, 
+#                            2: self.saturation, 
+#                            3: self.hue}
         
-        self.transform_list = [
-            self.adjust_brightness,
-            self.adjust_contrast,
-            self.adjust_saturation,
-            self.adjust_hue
-        ]
+#         self.transform_list = [
+#             self.adjust_brightness,
+#             self.adjust_contrast,
+#             self.adjust_saturation,
+#             self.adjust_hue
+#         ]
 
-    def adjust_brightness(self, input, params):
-        # https://kornia.readthedocs.io/en/latest/_modules/kornia/augmentation/functional/functional.html#apply_adjust_brightness
-        transformed = adjust_brightness(input, params['brightness_factor'].to(input.dtype) - 1)
-        return transformed
+#     def adjust_brightness(self, input, params):
+#         # https://kornia.readthedocs.io/en/latest/_modules/kornia/augmentation/functional/functional.html#apply_adjust_brightness
+#         transformed = adjust_brightness(input, params['brightness_factor'].to(input.dtype) - 1)
+#         return transformed
 
-    def adjust_contrast(self, input, params):
-        transformed = adjust_contrast(input, params['contrast_factor'].to(input.dtype))
-        return transformed
+#     def adjust_contrast(self, input, params):
+#         transformed = adjust_contrast(input, params['contrast_factor'].to(input.dtype))
+#         return transformed
 
-    def adjust_saturation(self, input, params):
-        transformed = adjust_saturation(input, params['saturation_factor'].to(input.dtype))
-        return transformed
+#     def adjust_saturation(self, input, params):
+#         transformed = adjust_saturation(input, params['saturation_factor'].to(input.dtype))
+#         return transformed
 
-    def adjust_hue(self, input, params):
-        transformed = adjust_hue(input, params['hue_factor'].to(input.dtype) * 2 * pi)
-        return transformed
+#     def adjust_hue(self, input, params):
+#         transformed = adjust_hue(input, params['hue_factor'].to(input.dtype) * 2 * pi)
+#         return transformed
 
-    def forward(self, batch):
-        params = self.jitter_2d.__forward_parameters__(batch.shape, self.jitter_2d.p,
-                                                       self.jitter_2d.p_batch, self.jitter_2d.same_on_batch)
-        should_aug = params['batch_prob']
-        if should_aug.sum() == 0:
-            return batch
-        # we shouldn't need this context manager here-- but trying it due to VRAM overflow issues
-        with torch.no_grad():
-            # N C T H W -> N T C H W
-            outputs = batch.transpose(1, 2).contiguous().detach()
-            # print(outputs.shape)
-            for idx in params['order'].tolist():
-                if self.aug_values[idx] > 0:
-                    t = self.transform_list[idx]
-                    outputs[should_aug] = t(outputs[should_aug], params).detach()
-            outputs = outputs.transpose(1, 2).contiguous().detach()
-        return outputs
+#     def forward(self, batch):
+#         try:
+#             params = self.jitter_2d.__forward_parameters__(batch.shape, self.jitter_2d.p, self.jitter_2d.p_batch, self.jitter_2d.same_on_batch)
+#             should_aug = params['batch_prob']
+#         except Exception:
+#             # kornia 5.0.0
+#             should_aug = self.jitter_2d.__batch_prob_generator__(batch.shape, self.jitter_2d.p, self.jitter_2d.p_batch, self.jitter_2d.same_on_batch)
+        
+#         if should_aug.sum() == 0:
+#             return batch
+#         # we shouldn't need this context manager here-- but trying it due to VRAM overflow issues
+#         with torch.no_grad():
+#             # N C T H W -> N T C H W
+#             outputs = batch.transpose(1, 2).contiguous().detach()
+#             # print(outputs.shape)
+#             for idx in params['order'].tolist():
+#                 if self.aug_values[idx] > 0:
+#                     t = self.transform_list[idx]
+#                     outputs[should_aug] = t(outputs[should_aug], params).detach()
+#             outputs = outputs.transpose(1, 2).contiguous().detach()
+#         return outputs
 
 
-class RandomGrayscaleVideo(nn.Module):
-    """Wrapper to apply grayscaling to video clips on GPU
-    """
-    def __init__(self, p=0.5):
-        super().__init__()
-        self.p = p
+# class RandomGrayscaleVideo(nn.Module):
+#     """Wrapper to apply grayscaling to video clips on GPU
+#     """
+#     def __init__(self, p=0.5):
+#         super().__init__()
+#         self.p = p
 
-    def generate_parameters(self, batch_size: int):
-        p = torch.rand(batch_size)
-        should_aug = p < self.p
-        return should_aug
+#     def generate_parameters(self, batch_size: int):
+#         p = torch.rand(batch_size)
+#         should_aug = p < self.p
+#         return should_aug
 
-    def rgb_to_gray(self, batch):
-        # https://kornia.readthedocs.io/en/latest/_modules/kornia/color/gray.html#rgb_to_grayscale
-        r: torch.Tensor = batch[..., 0:1, :, :, :]
-        g: torch.Tensor = batch[..., 1:2, :, :, :]
-        b: torch.Tensor = batch[..., 2:3, :, :, :]
+#     def rgb_to_gray(self, batch):
+#         # https://kornia.readthedocs.io/en/latest/_modules/kornia/color/gray.html#rgb_to_grayscale
+#         r: torch.Tensor = batch[..., 0:1, :, :, :]
+#         g: torch.Tensor = batch[..., 1:2, :, :, :]
+#         b: torch.Tensor = batch[..., 2:3, :, :, :]
 
-        gray: torch.Tensor = 0.299 * r + 0.587 * g + 0.114 * b
-        gray = gray.repeat(1, 3, 1, 1, 1)
-        return gray
+#         gray: torch.Tensor = 0.299 * r + 0.587 * g + 0.114 * b
+#         gray = gray.repeat(1, 3, 1, 1, 1)
+#         return gray
 
-    def forward(self, batch):
-        should_aug = self.generate_parameters(batch.shape[0])
-        outputs = batch# .clone()
-        outputs[should_aug] = self.rgb_to_gray(outputs[should_aug]).detach()
-        return outputs
+#     def forward(self, batch):
+#         should_aug = self.generate_parameters(batch.shape[0])
+#         outputs = batch# .clone()
+#         outputs[should_aug] = self.rgb_to_gray(outputs[should_aug]).detach()
+#         return outputs
 
 
 class StackClipInChannels(nn.Module):
@@ -307,32 +323,44 @@ def get_gpu_transforms(augs: DictConfig, mode: str = '2d') -> dict:
     # input is a tensor of shape N x C x F x H x W
     train_transforms = [ToFloat()]
     val_transforms = [ToFloat()]
+    
+    kornia_transforms = []
+    
     if augs.LR > 0:
-        train_transforms.append(K.RandomHorizontalFlip3D(p=augs.LR,
-                                                         same_on_batch=False,
-                                                         return_transform=False))
+        kornia_transforms.append(K.RandomHorizontalFlip(p=augs.LR,
+                                                        same_on_batch=False,
+                                                        return_transform=False))
     if augs.UD > 0:
-        train_transforms.append(K.RandomVerticalFlip3D(p=augs.UD,
-                                                       same_on_batch=False, return_transform=False))
+        kornia_transforms.append(K.RandomVerticalFlip(p=augs.UD,
+                                                     same_on_batch=False, return_transform=False))
     if augs.degrees > 0:
-        train_transforms.append(K.RandomRotation3D((1e-7, 1e-7, augs.degrees)))
+        kornia_transforms.append(K.RandomRotation(augs.degrees))
 
     if augs.brightness > 0 or augs.contrast > 0 or augs.saturation > 0 or augs.hue > 0:
-        train_transforms.append(RandomColorJitterVideo(brightness=augs.brightness,
-                                                       contrast=augs.contrast,
-                                                       saturation=augs.saturation,
-                                                       hue=augs.hue,
-                                                       p=augs.color_p,
-                                                       same_on_batch=False,
-                                                       return_transform=False))
-
+        kornia_transforms.append(K.ColorJitter(brightness=augs.brightness,
+                                              contrast=augs.contrast, 
+                                              saturation=augs.saturation, 
+                                              hue=augs.hue, 
+                                              p=augs.color_p, 
+                                              same_on_batch=False, 
+                                              return_transform=False))
     if augs.grayscale > 0:
-        train_transforms.append(RandomGrayscaleVideo(p=augs.grayscale))
+        kornia_transforms.append(K.RandomGrayscale(p=augs.grayscale))
+    
     
     norm = NormalizeVideo(mean=augs.normalization.mean,
                           std=augs.normalization.std)
-    train_transforms.append(norm)
-    val_transforms.append(norm)
+    # kornia_transforms.append(norm)
+    
+    kornia_transforms = VideoSequential(*kornia_transforms, 
+                                        data_format='BCTHW', 
+                                        same_on_frame=True)    
+    
+    train_transforms = [ToFloat(), 
+                        kornia_transforms, 
+                        norm]
+    val_transforms = [ToFloat(), 
+                      norm]
 
     denormalize = []
     if mode == '2d':
